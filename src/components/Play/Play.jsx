53efea58e909449db7ef51b8753456d1fa7b5f0a
Plut0r <ogunolaololade@gmail.com>
2023-07-22 12:58:19 +0100

    play/pause functionality

--- src/components/Play/Play.jsx
+++ src/components/Play/Play.jsx
@@ -1,34 +1,66 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function Play(props) {
+  // console.log(props.isPlaying);
   //  const [value, setValue] = useState(80);
   return (
     <div className="w-full h-20 play fixed left-0 right-0 bottom-0 flex items-center justify-between py-10 pl-5 lg:pl-20 pr-10">
-      <audio src={props.song[props.currentSongIndex].audio} ref={props.audioEl} />
+      <audio
+        src={props.song[props.currentSongIndex].audio}
+        ref={props.audioEl}
+      />
       <div className="h-[49px] w-48 flex gap-3">
         <div className="rounded-xl">
-          <img className="w-[49px] h-[49px] rounded-xl object-cover" src={props.song[props.currentSongIndex].image} alt="user" />
+          <img
+            className="w-[49px] h-[49px] rounded-xl object-cover"
+            src={props.song[props.currentSongIndex].image}
+            alt="user"
+          />
         </div>
         <div className="text-white flex flex-col mt-2">
-          <h4 className="text-sm font-bold">{props.song[props.currentSongIndex].title}</h4>
-          <p className="text-white/50 font-bold text-xs">{props.song[props.currentSongIndex].artist}</p>
+          <h4 className="text-sm font-bold">
+            {props.song[props.currentSongIndex].title}
+          </h4>
+          <p className="text-white/50 font-bold text-xs">
+            {props.song[props.currentSongIndex].artist}
+          </p>
         </div>
       </div>
       <div className="flex items-center gap-2 lg:hidden">
         <div onClick={() => props.setIsplaying(!props.isPlaying)}>
-        <img src="/images/play.png" alt="play" />
+          {props.isPlaying ? (
+            <img src="/images/play.png" alt="play" />
+          ) : (
+            <div className="text-white text-xs w-6 h-6 rounded-full border border-white flex items-center justify-center">||</div>
+          )}
         </div>
         <div onClick={() => props.skipSong(true)}>
-        <img src="/images/next.png" alt="next" />
+          <img src="/images/next.png" alt="next" />
         </div>
       </div>
       <div className="hidden lg:flex flex-col items-center">
         <div className="flex items-center gap-5 cursor-pointer">
-          <div onClick={props.shuffle}><img src="/images/shuffle.png" alt="" /></div>
-          <div onClick={() => props.skipSong(false)}><img src="/images/previous.png" alt="" /></div>
-          <div onClick={() => props.setIsplaying(!props.isPlaying)}><img src="/images/play.png" alt="" /></div>
-          <div onClick={() => props.skipSong(true)}><img src="/images/next.png" alt="" /></div>
-          <div onClick={props.repeate}><img src="/images/repeate-one.png" alt="" /></div>
+          <div onClick={props.shuffle}>
+            <img src="/images/shuffle.png" alt="" />
+          </div>
+          <div onClick={() => props.skipSong(false)}>
+            <img src="/images/previous.png" alt="" />
+          </div>
+          <div onClick={() => props.setIsplaying(!props.isPlaying)}>
+            {props.isPlaying ? (
+              <img src="/images/play.png" alt="play" />
+            ) : (
+              <div className="text-white text-xs w-6 h-6 rounded-full border border-white flex items-center justify-center">
+                ||
+              </div>
+            )}
+          </div>
+          <div onClick={() => props.skipSong(true)}>
+            <img src="/images/next.png" alt="" />
+          </div>
+          <div onClick={props.repeate}>
+            <img src="/images/repeate-one.png" alt="" />
+          </div>
         </div>
         <div className="slider-container">
           <img className="" src="/images/slider.png" alt="" />
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,43 +2,43 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const images = [
-    [
-        {
-            id: 1,
-            src: "/images/Home.png",
-            alt: "home-icon",
-            to: "/",
-        },
-        {
-            id: 2,
-            src: "/images/playlist.png",
-            alt: "playlist-icon",
-            to: "/collection",
-        }, 
-        {
-            id: 3,
-            src: "/images/radio.png",
-            alt: "radio-icon",
-        },
-        {
-            id: 4,
-            src: "/images/videos.png",
-            alt: "videos-icon",
-        },
-    ],
-    [
-        {
-            id: 5,
-            src: "/images/profile.png",
-            alt: "profile-icon",
-        }, 
-        {
-            id: 6,
-            src: "/images/Logout.png",
-            alt: "logout-icon",
-        },
-    ],
-]
+  [
+    {
+      id: 1,
+      src: "/images/Home.png",
+      alt: "home-icon",
+      to: "/",
+    },
+    {
+      id: 2,
+      src: "/images/playlist.png",
+      alt: "playlist-icon",
+      to: "/collection",
+    },
+    {
+      id: 3,
+      src: "/images/radio.png",
+      alt: "radio-icon",
+    },
+    {
+      id: 4,
+      src: "/images/videos.png",
+      alt: "videos-icon",
+    },
+  ],
+  [
+    {
+      id: 5,
+      src: "/images/profile.png",
+      alt: "profile-icon",
+    },
+    {
+      id: 6,
+      src: "/images/Logout.png",
+      alt: "logout-icon",
+    },
+  ],
+];
 
 function Sidebar() {
   return (
@@ -46,18 +46,18 @@ function Sidebar() {
       <div>
         <img src="/images/logo.svg" alt="logo" />
       </div>
-          <div className="bg-darkAlt rounded-[32px] w-[52px] py-4 flex flex-col items-center gap-7">
-              {images[0].map((image) => (
-                  <Link key={image.id} to={image.to}>
-                      <img src={image.src} alt={image.alt} />
-                  </Link>
-              ))}
-          </div>
-          <div className="bg-darkAlt rounded-[32px] w-[52px] py-4 flex flex-col items-center gap-7">
-              {images[1].map((image) => (
-                  <img key={image.id} src={image.src} alt={image.alt} />
-              ))}
-          </div>
+      <div className="bg-darkAlt rounded-[32px] w-[52px] py-4 flex flex-col items-center gap-7">
+        {images[0].map((image) => (
+          <Link key={image.id} to={image.to}>
+            <img src={image.src} alt={image.alt} />
+          </Link>
+        ))}
+      </div>
+      <div className="bg-darkAlt rounded-[32px] w-[52px] py-4 flex flex-col items-center gap-7">
+        {images[1].map((image) => (
+          <img key={image.id} src={image.src} alt={image.alt} />
+        ))}
+      </div>
     </div>
   );
 }
